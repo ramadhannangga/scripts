68cc71d05e6fb1a3c17da0117012cc5b7b6e4271
ramadhannangga <ramadhananggayudhanto@gmail.com>
2021-07-06 20:48:50 +0700

    initial for build iRISxe thanks to xyzuan for this

--- build.sh
+++ build.sh
@@ -1,33 +1,5 @@
 #!/usr/bin/env bash
-# install packages
-
-apt-get update -qq && \
-apt-get upgrade -y && \
-apt-get install --no-install-recommends -y \
-            binutils-dev \
-            bison \
-            ca-certificates \
-            ccache \
-            clang \
-            cmake \
-            curl \
-            file \
-            flex \
-            gcc \
-            g++ \
-            git \
-            libelf-dev \
-            libssl-dev \
-            make \
-            ninja-build \
-            python3 \
-            texinfo \
-            u-boot-tools \
-            xz-utils \
-            zlib1g-dev
-   
-# git clone   
-            
+# Use iRISxe build script as LLVM Build Script.
 git clone https://github.com/ramadhannangga/tc_build $(pwd)/tc_build
 cd $(pwd)/tc_build
 bash build-tc.sh
