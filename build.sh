8d577911d35e0baedfd95c93b91eee2626c0f6c0
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-07-08 18:45:05 +0700

    Scripts: upstream source llvm-project

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 # git clone llvm-project from llvm and git push to my github.
 git config --global user.email ramadhananggayudhanto@gmail.com
 git config --global user.name ramadhannangga
-git clone https://github.com/llvm/llvm-project llvm-project
+git clone https://ramadhannangga:$GH_TOKEN@github.com/ramadhannangga/llvm-project
 cd llvm-project
-git fetch https://github.com/NusantaraDevs/llvm-project
-git cherry-pick 33402aeb52e59392d8f6cb23b0bbe4dc64962860 --signoff
-git remote set-url origin https://ramadhannangga:$GH_TOKEN@github.com/ramadhannangga/llvm-project
+git fetch https://github.com/llvm/llvm-project
+git merge FETCH_HEAD
 git push --force origin main
