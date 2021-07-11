38338bb3770f3588b3b57c6953a0666965e2fe8f
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-07-11 17:07:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ apt-get update -qq && \
 apt-get upgrade -y && \
 apt-get install --no-install-recommends -y \
 apt install -y lsb-release wget software-properties-common \
-./llvm.sh 13
+bash llvm.sh 13
+git clone https://github.com/ramadhannangga/build --depth=1 build
 git clone https://github.com/ramadhannangga/llvm-project -b release/12.x --depth=1 llvm-project
 git clone https://github.com/bminor/binutils-gdb -b binutils-2_37-branch --depth=1 llvm-project/llvm/tools/binutils
 cd build || exit 1
