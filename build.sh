acdc077933c8df32054dd6140d81aea5f3035c40
Ramadhan Angga <76957390+ramadhannangga@users.noreply.github.com>
2021-07-11 17:17:29 +0700

    fix script !!! #2

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ git clone https://github.com/ramadhannangga/build --depth=1 build
 git clone https://github.com/ramadhannangga/llvm-project -b release/12.x --depth=1 llvm-project
 git clone https://github.com/bminor/binutils-gdb -b binutils-2_37-branch --depth=1 llvm-project/llvm/tools/binutils
 cd build || exit 1
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
 bash build_dtc 12.0
 export TOOLCHAIN_ROOT="$(dirname "$(pwd)")"
 export DTC_VERSION=12.0
@@ -22,6 +24,6 @@ chmod -R 777 $PREFIX_PATH
 git init
 git checkout -b 12.0
 git add .
-git commit -m "$DTC_VERSION-NusantaraDevsTC-$(date +'%d%m%y')" --signoff
-git remote add origin https://ramadhannangga:$GH_TOKEN@github.com/ramadhannangga/iRISxe-Clang
+git commit -m "$DTC_VERSION-iRISxeTC-$(date +'%d%m%y')" --signoff
+git remote add origin https://ramadhannangga:$GH_TOKEN@github.com/ramadhannangga/iRISxe-Clang.gi
 git push --force origin 12.0
